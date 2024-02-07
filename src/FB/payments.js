9b2cfb1a99819fa318dd9ce8bf78755dc452540a
Abdulla <smdabdulla510@gmail.com>
2024-02-07 17:20:48 +0530

    adjusted bc

--- src/FB/payments.js
+++ src/FB/payments.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function Payments() {
+
   const tableStyle = {
     width: '70%',
     margin: 'auto',
@@ -20,6 +21,10 @@ export default function Payments() {
     textAlign: 'left',
   };
 
+  const theROW = {
+    background: '#f8fafc',
+  };
+
   const totalCellStyle = {
     ...tdCellStyle,
     fontSize: '18px',
@@ -33,12 +38,12 @@ export default function Payments() {
       <div style={tableStyle}>
         <table style={{ width: '100%' }}>
           <thead>
-            <tr>
+            <tr style={theROW}>
               <th style={thCellStyle} colSpan={4}>Delegate</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr style={theROW}>
               <th style={thCellStyle} data-label="Particulars">Particulars</th>
               <th style={thCellStyle} data-label="No. of Persons">No. of Persons</th>
               <th style={thCellStyle} data-label="Amount (Rs.)">Amount (Rs.)</th>
@@ -50,28 +55,28 @@ export default function Payments() {
               <td style={tdCellStyle} data-label="Amount (Rs.)">12711</td>
               <td style={tdCellStyle} data-label="Total (Rs.)">1212</td>
             </tr>
-            <tr>
+            <tr style={theROW}>
               <th style={thCellStyle} colSpan={4} data-label="Particulars">Accompanying Persons</th>
             </tr>
-            <tr>
+            <tr style={theROW}>
               <th style={thCellStyle} data-label="Particulars"></th>
               <th style={thCellStyle} data-label="No. of Persons"></th>
               <th style={thCellStyle} data-label="Amount (Rs.)">Subtotal</th>
               <th style={thCellStyle} data-label="Total (Rs.)">1221.90</th>
             </tr>
-            <tr>
+            <tr style={theROW}>
               <th style={thCellStyle} data-label="Particulars"></th>
               <th style={thCellStyle} data-label="No. of Persons"></th>
               <th style={thCellStyle} data-label="Amount (Rs.)">SGST(9%)</th>
               <th style={thCellStyle} data-label="Total (Rs.)">2121.80</th>
             </tr>
-            <tr>
+            <tr style={theROW}>
               <th style={thCellStyle} data-label="Particulars"></th>
               <th style={thCellStyle} data-label="No. of Persons"></th>
               <th style={thCellStyle} data-label="Amount (Rs.)">CGST (9%)</th>
               <th style={thCellStyle} data-label="Total (Rs.)">1211.08</th>
             </tr>
-            <tr>
+            <tr style={theROW}>
               <th style={thCellStyle} data-label="Particulars"></th>
               <th style={thCellStyle} data-label="No. of Persons"></th>
               <th style={totalCellStyle} data-label="Amount (Rs.)">TOTAL</th>
@@ -80,7 +85,7 @@ export default function Payments() {
           </tbody>
         </table>
       </div>
-      <br></br>
+      <br />
       <button className="ui green button">Make Payment and Complete Registration</button>
     </div>
   );
